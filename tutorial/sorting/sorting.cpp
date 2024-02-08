34d2014ebf28a5dbc804a0f4d37966e4e176a2e8
Asif Uddin <uasif13@gmail.com>
2024-02-07 22:52:33 -0500

    sorting

--- tutorial/sorting/sorting.cpp
+++ tutorial/sorting/sorting.cpp
@@ -1,51 +1,54 @@
 #include <iostream>
 #include "sorting.h"
 
-int * insert_sort(int*& new_arr, const int*& old_arr, const int length) {
-//     // Size of arr can be found with .size() - https://cplusplus.com/reference/array/array/size/
-//     int * sorted = new int[arr.size()];
-//     int index = 0;
-//     for (int i = 0; i < arr.size(); ++i) {
-//         sorted[index] = arr[i];
-//         int j = 0;
-//         while(j < index) {
-//             if (sorted[j] > arr[i]) {
-//                 swap(sorted,j,index);
-//                 j = index;
-//             }
-//             else {
-//                 j += 1;
-//             }
-//         }
-//     }
-//     return sorted;
-}
-
-void print_arr(int*& arr, const int length) {
-    for (int i = 0; i < length; ++i) {
-        std::cout << *(arr+i) << " ";
-    }    
-}
-
-const int get_length(const int *& arr) {
-    int length = 0;
-    while (arr+length != nullptr) {
-        int a = *(arr+length);
-        length += 1;
-    }
-    return length;
-}
-
+// const int get_length(const int *& arr);
+int * sort(int * arr,int * sorted, const int length);
+void print_arr(int* arr, const int length);
 
 int main() {
     // Cannot use initializer list for pointers. 
     // Use new operator or other variable.
-    const int foo[] = {1,3,2,5,4};
-    const int * arr = foo;
-    
-    const int length = get_length(arr);
-    int * sorted_arr;
-    sorted_arr = insert_sort(sorted_arr, arr, length);
-    print_arr(sorted_arr,length);
+    const int length = 5;
+    int a [length];
+    int b [length] = {1,3,2,5,4};
+    int *g;
+    // Rest are zeros.
+    int c [length] = {3,1,2};
+    int d [length] = {};
+    int e [] = {6,8,3,2,5};
+    int f [] {6,5,3,4};
+    int * sorted;
+    // const int length = get_length(arr);
+    g = sort(e, sorted, length);
+    print_arr(g,length);
     return 0;
+}
+
+int* sort(int * arr,int * sorted, const int length) {
+//     // Size of arr can be found with .size() - https://cplusplus.com/reference/array/array/size/
+    // int * sorted = new int[arr.size()];
+    // In gdb, use p * array@len to get the values of the array
+    int j = 0;
+    int min = INT16_MAX;
+    int min_index = 0;
+    while (j < length) {
+        for (int i = 0; i < length; ++i) {
+            if (arr[i] < min) {
+                min = arr[i];
+                min_index = i;
+            }
+        }
+        sorted[j] = min;
+        min = INT16_MAX;
+        arr[min_index]=INT16_MAX;
+        j += 1;
+    }
+    return sorted;
+}
+
+void print_arr(int* arr, const int length) {
+    for (int i = 0; i < length; ++i) {
+        std::cout << *(arr+i) << " ";
+    }
+    std::cout << std::endl;    
 }
--- tutorial/sorting/sorting.h
+++ tutorial/sorting/sorting.h
@@ -2,7 +2,7 @@
 #ifndef SORTING_H
 #define SORTING_H
 
-int * insert_sort(int*& new_arr, const int*& old_arr, const int length);
-void print_arr(const int*& arr, const int length);
+int * insert_sort(int* new_arr, int* old_arr, const int length);
+void print_arr(int* arr, const int length);
 
-#endif SORTING_H
+#endif
